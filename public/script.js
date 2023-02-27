7d32d6fd798f41324b4d5754ebefd80fb09aec0d
Sandil Adhikari <adhikarisandil32@gmail.com>
2023-02-27 22:47:15 +0545

    routing

--- public/script.js
+++ public/script.js
@@ -9,3 +9,17 @@ let onClickX = function(){
     overallSidenavClassLists.remove("side-popup-show");
     overallSidenavClassLists.add("side-popup-hide");
 }
+
+let route = function(e, page){
+    e.preventDefault();
+    if(page !== 'index'){
+        let pathname = window.location.origin+"/public/pages/"+page+".html";
+        window.location.href = pathname;
+        // history.pushState({},"",pathname);
+    }
+    else{
+        let pathname = window.location.origin+"/public/"+page+".html";
+        window.location.href = pathname;
+        // history.pushState({},"",pathname);
+    }
+}
